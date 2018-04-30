f16b5196d481dfc33d2b758c59cce44017d33249
ywlee <ywlee@j-compia.com>
2018-04-30 14:27:56 +0900

    [ kotlin study 1 ] 버튼 추가 및 클릭 리스너 삽입

--- study1/app/src/main/java/ywlee/com/kotlinstudy1/MainActivity.kt
+++ study1/app/src/main/java/ywlee/com/kotlinstudy1/MainActivity.kt
@@ -2,11 +2,16 @@ package ywlee.com.kotlinstudy1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_changetext.setOnClickListener{
+            txv_hello.text = "Hello Kotlin !"
+        }
     }
 }
